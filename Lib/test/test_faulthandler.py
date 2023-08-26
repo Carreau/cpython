41d37fbdb5f3d9678be464c5ddbd17427f2e7e2f
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-08-26 21:46:30 +0200

    try to fix faulthandler

--- Lib/test/test_faulthandler.py
+++ Lib/test/test_faulthandler.py
@@ -41,11 +41,13 @@ def skip_segfault_on_android(test):
 
 @contextmanager
 def temporary_filename():
-    filename = tempfile.mktemp()
+    file = tempfile.NamedTemporaryFile(delete=False)
+    file.close()
+
     try:
-        yield filename
+        yield file.name
     finally:
-        os_helper.unlink(filename)
+        os_helper.unlink(file.name)
 
 class FaultHandlerTests(unittest.TestCase):
 
